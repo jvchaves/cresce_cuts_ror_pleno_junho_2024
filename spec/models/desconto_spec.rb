bd876c8fd71172d8730551d2796756fcbe4cd60a
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-06-04 15:14:58 -0300

    Test: add teste para descontos validos

--- spec/models/desconto_spec.rb
+++ spec/models/desconto_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Desconto, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validacoes Desconto' do
+    it 'Invalida' do
+      desconto = Desconto.new(nome: '')
+
+      expect(desconto.valid?).to be_falsy
+    end
+
+    it 'Valida' do
+      produto = Produto.new(nome: 'Produto 1')
+      desconto = Desconto.new(nome: 'Desconto 1', tipo: "Leve + Pague -", ativacao: Time.now, produto: produto)
+
+      expect(desconto.valid?).to be_truthy
+    end
+  end
 end
