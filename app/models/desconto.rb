9fa930ab9d40c398788be19d5574d6daf4c131c1
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-06-04 16:27:24 -0300

    Feat: add enum para tipos de promocao

--- app/models/desconto.rb
+++ app/models/desconto.rb
@@ -1,4 +1,6 @@
 class Desconto < ApplicationRecord
   validates :nome, :tipo, :ativacao, presence: true
   belongs_to :produto
+
+  enum tipo: { de_por: "DE / POR", leve_mais_pague_menos: "Leve + Pague -", percentual: "Percentual" }
 end
