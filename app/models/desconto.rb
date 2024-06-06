0146c57413832f74e8c093882bafc19ff4137e4d
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-06-06 14:10:00 -0300

    Feat: add validate com mensagens personalizadas

--- app/models/desconto.rb
+++ app/models/desconto.rb
@@ -1,6 +1,19 @@
 class Desconto < ApplicationRecord
-  validates :nome, :tipo, :ativacao, presence: true
   belongs_to :produto
 
   enum tipo: { de_por: "DE / POR", leve_mais_pague_menos: "Leve + Pague -", percentual: "Percentual" }
+
+  validate on: :create do
+    campos_obrigatorios_preenchidos?
+	end
+
+  private
+
+  def campos_obrigatorios_preenchidos?
+    errors.add(:nome, "Precisa ser preenchido") if nome.blank?
+    errors.add(:tipo, "Precisa ser preenchido") if tipo.blank?
+    errors.add(:ativacao, "Precisa ser preenchido") if ativacao.blank?
+    errors.add(:preco, "Precisa ser preenchido") if preco.blank?
+    errors.add(:base, "Desconto não é válido") if errors.any?
+  end
 end
