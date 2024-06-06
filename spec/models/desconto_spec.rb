492983a80858cb1470a2edf695b46cf3f3d2b4a5
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-06-06 14:35:28 -0300

    teste: adiciono cobertura para o tipo de desconto de-por

--- spec/models/desconto_spec.rb
+++ spec/models/desconto_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Desconto, type: :model do
-  let(:produto) { Produto.create(nome: "Produto Teste") } 
+  let(:produto) { Produto.create(nome: "Produto Teste") }
 
   context "validações de presença" do
     it "é válido com todos os campos preenchidos" do
@@ -10,6 +10,8 @@ RSpec.describe Desconto, type: :model do
         tipo: "de_por",
         ativacao: Date.today,
         preco: 10.0,
+        preco_de: 20.0,
+        preco_por: 15.0,
         produto: produto
       )
       expect(desconto).to be_valid
@@ -20,6 +22,8 @@ RSpec.describe Desconto, type: :model do
         tipo: "de_por",
         ativacao: Date.today,
         preco: 10.0,
+        preco_de: 20.0,
+        preco_por: 15.0,
         produto: produto
       )
       expect(desconto).not_to be_valid
@@ -57,6 +61,47 @@ RSpec.describe Desconto, type: :model do
       )
       expect(desconto).not_to be_valid
       expect(desconto.errors[:preco]).to include("Precisa ser preenchido")
+    end
+  end
+
+  context "Descontos do tipo DE-POR" do
+    it "é válido com preço de e preço por" do
+      desconto = Desconto.new(
+        nome: "Desconto Teste",
+        tipo: "de_por",
+        ativacao: Date.today,
+        preco: 10.0,
+        preco_de: 20.0,
+        preco_por: 15.0,
+        produto: produto
+      )
+      expect(desconto).to be_valid
+    end
+
+    it "não é válido sem preço de" do
+      desconto = Desconto.new(
+        nome: "Desconto Teste",
+        tipo: "de_por",
+        ativacao: Date.today,
+        preco: 10.0,
+        preco_por: 15.0,
+        produto: produto
+      )
+      expect(desconto).not_to be_valid
+      expect(desconto.errors[:preco_de]).to include("Precisa ser preenchido")
+    end
+
+    it "não é válido sem preço por" do
+      desconto = Desconto.new(
+        nome: "Desconto Teste",
+        tipo: "de_por",
+        ativacao: Date.today,
+        preco: 10.0,
+        preco_de: 20.0,
+        produto: produto
+      )
+      expect(desconto).not_to be_valid
+      expect(desconto.errors[:preco_por]).to include("Precisa ser preenchido")
     end
   end
 end
