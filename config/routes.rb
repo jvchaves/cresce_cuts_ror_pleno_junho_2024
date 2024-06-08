5ee85032c804179d8c48ef7f6848c1bc8d0887e8
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-06-08 08:55:23 -0300

    Chore: altero rota route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "descontos#index"
 
   namespace :api do
     namespace :v1 do
